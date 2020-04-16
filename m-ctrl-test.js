6a6f69d32e89a2a51a3f82b051851c3acb501775
Ben Zekri <mohamed.ben-zekri@suezenvironnement.com>
2020-04-16 02:03:48 +0200

    Initial Commit

---
+++ m-ctrl-test.js
@@ -0,0 +1,27 @@
+'use mode strict';
+
+MControl.register(
+    class MCtrlCookbook extends MControl.Controller {
+        constructor(element) {
+            super(element)
+            this.model.name  = "Noa"
+            this.model.number = 0
+            this.model.city = "Rio"
+            this.model.browser = "Chrome"
+            this.model.color = "red"
+            this.model.colors = ['red','green','blue','yellow','grey','white','black','purple']
+        }
+        init() { 
+            setInterval(_ => {
+                const index = Math.floor(this.model.colors.length*Math.random())
+                this.model.color = this.model.colors[index]
+            },400)
+        }
+        inc() { this.model.number += 1}
+        dec() { this.model.number -= 1}
+        newname(name) {
+            this.model.name = name
+        }
+    }
+)
+
